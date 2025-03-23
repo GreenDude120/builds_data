fc4a3914b67d38b2d42185d5d933b4bc85625166
D <igothippie@yahoo.com>
2025-03-23 12:32:34 -0400

    add current python to repo

---
+++ DoItAll3v3.py
@@ -0,0 +1,31 @@
+from doitallv3 import GetAllCharData, MakeHome, GetNonZon, GetUniqueProjectiles, GetBong, GetDashers, GetChargers, GetOffensiveAuraItemsEquipped, MakeClassPages, GitHubSync
+
+#GetAllCharData()
+#GetClassCharData() ## No longer needed
+#MakeHome()
+GetDashers() 
+GetNonZon()
+GetUniqueProjectiles()
+GetBong() #-
+GetChargers() #-
+GetOffensiveAuraItemsEquipped() #-
+MakeClassPages() #-
+GitHubSync()
+ 
+print("✅ All scripts executed successfully!")
+
+from doitallhcv3 import GetAllHCCharData, MakehcHome, GethcNonZon, GethcUniqueProjectiles, GethcBong, GethcDashers, GethcChargers, GethcOffensiveAuraItemsEquipped, MakehcClassPages, GitHubSync
+
+#GetAllHCCharData()
+#GethcClassCharData() ## No longer needed
+#MakehcHome()
+#GethcDashers()
+#GethcNonZon()
+#GethcUniqueProjectiles()
+#GethcBong()
+#GethcChargers()
+#GethcOffensiveAuraItemsEquipped()
+#MakehcClassPages()
+#GitHubSync()
+
+print("✅ All Hardcore scripts executed successfully!")
